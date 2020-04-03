97a9ef89fba8a2449502c318a393fd279af6f2f5
Graham Christensen <graham@grahamc.com>
2020-04-03 13:31:45 -0400

    port to poetry2nix

---
+++ overrides.nix
@@ -0,0 +1,8 @@
+{ pkgs }:
+
+self: super: {
+  nixops = super.nixops.overridePythonAttrs({ nativeBuildInputs ? [], ... }: {
+    format = "pyproject";
+    nativeBuildInputs = nativeBuildInputs ++ [ self.poetry ];
+  });
+}
