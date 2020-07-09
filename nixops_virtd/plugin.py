420d9c18493831c2d0ea9980543b8a64362cc049
adisbladis <adisbladis@gmail.com>
2020-07-09 14:44:34 +0200

    Use the NixOps class interface from https://github.com/NixOS/nixops/pull/1383

--- nixops_virtd/plugin.py
+++ nixops_virtd/plugin.py
@@ -1,14 +1,20 @@
 import os.path
 import nixops.plugins
+from nixops.plugins import Plugin
+
+
+class NixopsLibvirtdPlugin(Plugin):
+    @staticmethod
+    def nixexprs():
+        return [os.path.dirname(os.path.abspath(__file__)) + "/nix"]
+
+    @staticmethod
+    def load():
+        return [
+            "nixops_virtd.backends.libvirtd",
+        ]
 
 
 @nixops.plugins.hookimpl
-def nixexprs():
-    return [os.path.dirname(__file__) + "/nix"]
-
-
-@nixops.plugins.hookimpl
-def load():
-    return [
-        "nixops_virtd.backends.libvirtd",
-    ]
+def plugin():
+    return NixopsLibvirtdPlugin()
